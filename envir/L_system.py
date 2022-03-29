1033a6f6cd5062508ee2b4a720d485213291b5ba
merlynr <lcq1013962426@gmail.com>
2022-03-29 16:52:44 +0800

    补充元素结点色阶

--- envir/L_system.py
+++ envir/L_system.py
@@ -9,11 +9,12 @@ def replacement(str, rules, order):
         str = dst
     return str
 
+
 def interpretation(str, actions, rotate, angle, x, y):
     import math
 
-    point_x = [x,]
-    point_y = [y,]
+    point_x = [x, ]
+    point_y = [y, ]
 
     for s in str:
         if s not in actions:
@@ -27,3 +28,30 @@ def interpretation(str, actions, rotate, angle, x, y):
             point_x.append(point_x[-1] + math.cos(r))
             point_y.append(point_y[-1] + math.sin(r))
     return point_x, point_y
+
+
+grammer = {
+    "start": "A",
+    "rules": {"A": "A-B--B+A++AA+B-", "B": "+A-BB--B-A++A+B"},
+}
+
+geometry = {
+    "rotate": 60,
+    "actions": {"+": "left", "-": "right", "A": "forward", "B": "forward"}
+}
+import pandas as pd
+
+str = replacement(grammer["start"], grammer["rules"], 3)
+x, y = interpretation(str, geometry["actions"], geometry["rotate"], 30, 0, 0)
+domData = pd.read_csv('dom.csv', usecols=['element', 'LOD'])
+'''
+添加默认值，使得两个list长度一样
+'''
+LOD = domData['LOD'].values.tolist()
+element = domData['element'].values.tolist()
+max_len = max(len(x), len(element))
+difference_len = max_len - len(element)
+LOD.extend('#' * difference_len)
+element.extend('@' * difference_len)
+dataframe = pd.DataFrame({'x': x, 'y': y, 'element': element, 'LOD': LOD})
+dataframe.to_csv("gosper.csv", sep=',')
--- envir/gosper_show.py
+++ envir/gosper_show.py
@@ -1,35 +1,37 @@
-# Gosper 曲线
-from envir.L_system import replacement, interpretation
-
-grammer = {
-    "start": "A",
-    "rules": {"A": "A-B--B+A++AA+B-", "B": "+A-BB--B-A++A+B"},
-}
-
-geometry = {
-    "rotate": 60,
-    "actions": {"+": "left", "-": "right", "A": "forward", "B": "forward"}
-}
-
 import matplotlib.pyplot as plt
 import sys
 import time
+import pandas as pd
 
-str = replacement(grammer["start"], grammer["rules"], 3)
-x, y = interpretation(str, geometry["actions"], geometry["rotate"], 30, 0, 0)
-
+data = pd.read_csv('gosper.csv')
 pos = {
     'x': 0.00,
     'y': 0.00
 }
-
+x=data['x']
+y=data['y']
+el = data['element']
+LOD = data['LOD']
 
 def showTrack(pos):
     print(pos)
 
 
+# class Gosper_Show:
+#     def __init__(self, data):
+#         '''
+#         定义环境元素
+#         '''
+#         self.x = data['x']
+#         self.y = data['y']
+#
+#         print(self.x[1:3])
+
+
+
 while True:
     plt.plot(x, y)
+
     price = input("price:[eg, 10,20]\n")
     a = price.split(',')
     if a[0] == 'end' and a[1] == 'end':
@@ -44,8 +46,20 @@ while True:
     plt.plot(m, n, color='r')
     plt.show()
 
+
+#运行轨迹是同层进行搜索
+
 # 进行数据绑定
 # 同层划分LOD
 # 加入条件判断，是否可以进行下一步
 # 进行移动，模糊定位
 # 记录移动轨迹，记录状态
+# 优化最少的步骤达到目的
+
+# **
+# LOD表示层级之间跳转
+# page表示页面跳转，可以实现不同层次之间跳跃
+
+# if __name__ == "__main__":
+#     print('main')
+#     Gosper_Show(data)
